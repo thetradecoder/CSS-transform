cf73181307f1db15908ccaf18f21f31ff0d8667e
unknown <thetradecoder@gmail.com>
2020-03-23 11:50:47 +0600

    Updates

---
+++ precache-manifest.b73384ad937b9e6edf959bccc0b4089c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a624347da6766e20a2be14af41eac81",
+    "url": "/css-transform/index.html"
+  },
+  {
+    "revision": "adacbc36fc000f9ac8fa",
+    "url": "/css-transform/static/css/main.e8058c66.chunk.css"
+  },
+  {
+    "revision": "b96945638ced5633f6bc",
+    "url": "/css-transform/static/js/2.d81e40d9.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/css-transform/static/js/2.d81e40d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adacbc36fc000f9ac8fa",
+    "url": "/css-transform/static/js/main.4218560c.chunk.js"
+  },
+  {
+    "revision": "9ed6bb636588b977b5fe",
+    "url": "/css-transform/static/js/runtime-main.3f815a2b.js"
+  }
+]);
